6be65ba8c99d2274f53de2a6f019c5a6aa1d4272
Illia <ilaryndin30@gmail.com>
2026-03-23 22:34:56 +0100

    feat: add about section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/sections/HeroSection";
 import ServicesSection from "@/components/sections/services/ServicesSection";
 import VipSection from "@/components/sections/VipSection";
 import CondolenceSection from "@/components/sections/CondolenceSection";
+import AboutSection from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <ServicesSection />
       <VipSection />
       <CondolenceSection />
+      <AboutSection />
     </main>
   );
 }
