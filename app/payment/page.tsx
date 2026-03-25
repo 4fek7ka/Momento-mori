dab008382eb8f0a07972287a941de4f221453428
Illia <ilaryndin30@gmail.com>
2026-03-25 06:49:01 +0100

    fix

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,6 +1,5 @@
 import HeaderTransparent from "@/components/layout/HeaderTransparent";
 import PaymentIntroSection from "@/components/sections/payment/PaymentIntroSection";
-import PaymentDetailsSection from "@/components/sections/payment/PaymentDetailsSection";
 import Footer from "@/components/layout/Footer";
 
 export default function PaymentPage() {
